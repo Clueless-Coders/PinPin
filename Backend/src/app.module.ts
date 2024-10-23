1c53ad83bbf8f3505472a38ec27dfea76e1749e8
trina314 <trina0dang@gmail.com>
2024-10-23 13:17:25 -0500

    initial file creations for pin module

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PinsModule } from './pins/pins.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PinsModule],
   controllers: [AppController],
   providers: [AppService],
 })
