79be8284bb9864048bdd333e10d1ab79ed3c6b5a
Caroline Peach <cpeach2@lsu.edu>
2024-11-20 15:48:35 -0600

    implementing scrollable flatlist and adding snap points

--- Mobile/app/(home)/Filters.tsx
+++ Mobile/app/(home)/Filters.tsx
@@ -1,9 +1,84 @@
-import { Text, View } from "react-native";
+import React, { useCallback, useRef, useMemo } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import BottomSheet, {
+  BottomSheetFlatList,
+  BottomSheetTextInput,
+} from "@gorhom/bottom-sheet";
 
-export default function Filters() {
-  return (
-    <View>
-      <Text>Filters Page.</Text>
-    </View>
+const App = () => {
+  // hooks
+  const sheetRef = useRef<BottomSheet>(null);
+
+  // variables
+  const data = useMemo(
+    () =>
+      Array(50)
+        .fill(0)
+        .map((_, index) => `index-${index}`),
+    []
   );
-}
+  const snapPoints = useMemo(() => ["15%", "75%", "100%"], []);
+
+  // callbacks
+  const handleSheetChange = useCallback((index: number) => {
+    console.log("handleSheetChange", index);
+  }, []);
+
+  // render
+  const renderItem = useCallback(
+    (something: any) => {
+      return (<View style={styles.itemContainer}>
+        <Text>{something.item}</Text>
+      </View>)
+    },
+    []
+  );
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        enableDynamicSizing={false}
+        onChange={handleSheetChange}
+      >
+        <BottomSheetTextInput style={styles.input} />
+        <BottomSheetFlatList
+          data={data}
+          keyExtractor={(i) => i}
+          renderItem={renderItem}
+          contentContainerStyle={styles.contentContainer}
+        />
+      </BottomSheet>
+    </GestureHandlerRootView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 200,
+  },
+  contentContainer: {
+    backgroundColor: "white",
+  },
+  itemContainer: {
+    padding: 6,
+    margin: 6,
+    backgroundColor: "#eee",
+  },
+  input: {
+    alignSelf: "stretch",
+    marginHorizontal: 10,
+    marginVertical: 5,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#000000",
+    fontSize: 16,
+    lineHeight: 20,
+    padding: 5,
+    color: "#FFF9ED",
+  },
+});
+
+export default App;
--- Mobile/app/(home)/index.tsx
+++ Mobile/app/(home)/index.tsx
@@ -1,64 +1,44 @@
 import { Link } from "expo-router";
-import { View, Text, StyleSheet, Button } from 'react-native';
-import React, { useCallback, useMemo, useRef } from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import BottomSheet, { BottomSheetModal, BottomSheetModalProvider, BottomSheetView } from '@gorhom/bottom-sheet';
+import { View, Text, StyleSheet } from "react-native";
+import React, { useCallback, useEffect, useMemo, useRef } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetTextInput,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+
+// TO DO
+// Pin List Component
+// Scrollable Pin List Component
+// Search Bar Component
+// Square Buttons Component
+// Modal Customizations
 
 //Map, create pin, etc
 export default function HomeIndex() {
-  // ref
-  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
-
-  // callbacks
-  const handlePresentModalPress = useCallback(() => {
-    bottomSheetModalRef.current?.present();
-  }, []);
-  const handleSheetChanges = useCallback((index: number) => {
-    console.log('handleSheetChanges', index);
-  }, []);
-
-
   return (
-    <GestureHandlerRootView style={styles.container}>
-
       <View>
-      <Text>Hello!</Text>
-      <Link href={"/(home)/Filters"}>Filters page</Link>
-      <Link href={"/(home)/NewPin"}>New Pin page</Link>
-      <Link href={"/(home)/Settings"}>Settings page</Link>
-      <Link href={"/(home)/0"}> PinDetail page</Link>
+        <Text>Hello!</Text>
+        <Link href={"/(home)/Filters"}>Filters page</Link>
+        <Link href={"/(home)/NewPin"}>New Pin page</Link>
+        <Link href={"/(home)/Settings"}>Settings page</Link>
+        <Link href={"/(home)/0"}> PinDetail page</Link>
       </View>
-      
-        <BottomSheetModalProvider>
-          <Button
-            onPress={handlePresentModalPress}
-            title="Present Modal"
-            color="black"
-          />
-          <BottomSheetModal
-            ref={bottomSheetModalRef}
-            onChange={handleSheetChanges}
-          >
-            <BottomSheetView style={styles.contentContainer}>
-              <Text>search</Text>
-            </BottomSheetView>
-        </BottomSheetModal>
-        </BottomSheetModalProvider>
-    </GestureHandlerRootView>
-
-    
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    justifyContent: 'center',
-    backgroundColor: '#FFF9ED',
+    padding: 20,
+    // justifyContent: "center",
+    backgroundColor: "#FFF9ED",
   },
   contentContainer: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
+    backgroundColor: "#FFF9ED",
   },
 });
