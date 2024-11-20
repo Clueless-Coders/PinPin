84fa115cac7b0e1c16b4be5b1c14e5a9b0f6b5d9
Caroline Peach <cpeach2@lsu.edu>
2024-11-20 08:56:00 -0600

    bottom modal setup

--- Mobile/app/(home)/index.tsx
+++ Mobile/app/(home)/index.tsx
@@ -1,15 +1,64 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useCallback, useMemo, useRef } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet, { BottomSheetModal, BottomSheetModalProvider, BottomSheetView } from '@gorhom/bottom-sheet';
 
 //Map, create pin, etc
 export default function HomeIndex() {
+  // ref
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
+
   return (
-    <View>
+    <GestureHandlerRootView style={styles.container}>
+
+      <View>
       <Text>Hello!</Text>
       <Link href={"/(home)/Filters"}>Filters page</Link>
       <Link href={"/(home)/NewPin"}>New Pin page</Link>
       <Link href={"/(home)/Settings"}>Settings page</Link>
       <Link href={"/(home)/0"}> PinDetail page</Link>
-    </View>
+      </View>
+      
+        <BottomSheetModalProvider>
+          <Button
+            onPress={handlePresentModalPress}
+            title="Present Modal"
+            color="black"
+          />
+          <BottomSheetModal
+            ref={bottomSheetModalRef}
+            onChange={handleSheetChanges}
+          >
+            <BottomSheetView style={styles.contentContainer}>
+              <Text>search</Text>
+            </BottomSheetView>
+        </BottomSheetModal>
+        </BottomSheetModalProvider>
+    </GestureHandlerRootView>
+
+    
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    backgroundColor: '#FFF9ED',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
