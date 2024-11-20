0f77f5269eeefd68222499e8012dd3488d6f3452
TristanD0703 <111524307+TristanD0703@users.noreply.github.com>
2024-11-20 16:16:28 -0600

    Add error handling + cleanup

--- Backend/src/pins/pins.service.ts
+++ Backend/src/pins/pins.service.ts
@@ -1,88 +1,104 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import {
+  ForbiddenException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { Pin } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 import { CreatePinDTO, UpdatePinDTO } from './dto/pins.dto';
 import { Request } from 'express';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class PinsService {
-    constructor(private readonly databaseService: DatabaseService) { }
+  constructor(private readonly databaseService: DatabaseService) {}
 
-    async create(createPin: CreatePinDTO, req?: Request) {
-        return this.databaseService.pin.create({
-            data: {
-                text: createPin.text,
-                userID: req['user'].id,
-                imageURL: createPin.imageURL, // This will be optional
-                longitude: createPin.longitude,
-                latitude: createPin.latitude,
-                downvotes: 0,
-                upvotes: 0
-            }
-        });
+  async create(createPin: CreatePinDTO, req: Request) {
+    try {
+      return await this.databaseService.pin.create({
+        data: {
+          text: createPin.text,
+          userID: req['user'].id,
+          imageURL: createPin.imageURL, // This will be optional
+          longitude: createPin.longitude,
+          latitude: createPin.latitude,
+          downvotes: 0,
+          upvotes: 0,
+        },
+      });
+    } catch (e) {
+      throw new InternalServerErrorException('Pin create failed.', e);
+    }
+  }
+
+  // async getAllPins() {
+  //     return this.databaseService.pin.findMany();
+  // }
+
+  async getPin(pinID: number) {
+    let res: Pin;
+    try {
+      res = await this.databaseService.pin.findUnique({
+        where: {
+          id: pinID,
+        },
+      });
+    } catch (e) {
+      throw new InternalServerErrorException('DB call failed');
     }
 
-    // async getAllPins() {
-    //     return this.databaseService.pin.findMany();
-    // }
+    if (!res)
+      throw new NotFoundException('Pin with ID ' + pinID + ' not found.');
+    return res;
+  }
 
-    async getPin(pinID: number) {
-        return this.databaseService.pin.findUnique({
-            where: {
-                id: pinID,
-            }
-        });
-    }
+  async updatePin(pinID: number, updatePinDTO: UpdatePinDTO, req?: Request) {
+    const currUser = req['user'];
+    const pinToEdit = await this.getPin(pinID);
 
-    async updatePin(pinID: number, updatePinDTO: UpdatePinDTO, req?: Request) {
-        const currUser = req['user'];
-        const pinToEdit = await this.getPin(pinID);
+    if (currUser.id !== pinToEdit.userID)
+      throw new ForbiddenException('User can only edit their own pin');
 
-        if (currUser.id !== pinToEdit.userID)
-            throw new ForbiddenException("User can only edit their own pin");
+    return this.databaseService.pin.update({
+      where: {
+        id: pinID,
+      },
+      data: updatePinDTO,
+    });
+  }
 
-        return this.databaseService.pin.update({
-            where: {
-                id: pinID,
-            },
-            data: updatePinDTO,
-        });
-    }
+  async patchUpvote(pinID: number, increment: number) {
+    // return this.databaseService.pin.update({
+    //     where: {
+    //         id: pinID,
+    //     },
+    //     data: { upvotes: { increment: increment } },
+    // });
+  }
 
-    async patchUpvote(pinID: number, increment: number) {
-        // return this.databaseService.pin.update({
-        //     where: {
-        //         id: pinID,
-        //     },
-        //     data: { upvotes: { increment: increment } },
-        // });
-    }
+  async patchDownvote(pinID: number, increment: number) {
+    // return this.databaseService.pin.update({
+    //     where: {
+    //         id: pinID,
+    //     },
+    //     data: { downvotes: { increment: increment } },
+    // });
+  }
 
-    async patchDownvote(pinID: number, increment: number) {
-        // return this.databaseService.pin.update({
-        //     where: {
-        //         id: pinID,
-        //     },
-        //     data: { downvotes: { increment: increment } },
-        // });
-    }
+  async removePin(pinID: number, req?: Request) {
+    const currUser = req['user'];
+    const pinToDelete = await this.getPin(pinID);
 
-    async removePin(pinID: number, req?: Request) {
-        const currUser = req['user'];
-        const pinToDelete = await this.getPin(pinID);
+    if (pinToDelete.userID !== currUser.id)
+      throw new ForbiddenException('User can only delete their own pins');
 
-        if (pinToDelete.userID !== currUser.id)
-            throw new ForbiddenException("User can only delete their own pins");
-
-        return await this.databaseService.pin.delete({
-            where: {
-                id: pinID,
-                userID: currUser.id,
-            }
-        });
-
-    }
+    return await this.databaseService.pin.delete({
+      where: {
+        id: pinID,
+        userID: currUser.id,
+      },
+    });
+  }
 }
 // @Get()
 // getAllPins(){
