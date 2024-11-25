b7d361f57a0911e9a5ae1df1684619893e8ef3b6
Brandon <blede14@lsu.edu>
2024-11-25 15:52:43 -0600

    Testing?

--- Mobile/app/(home)/index.tsx
+++ Mobile/app/(home)/index.tsx
@@ -1,15 +1,61 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import * as Location from 'expo-location';
 
 //Map, create pin, etc
 export default function HomeIndex() {
+  
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let display_location = 'Waiting...';
+  if (errorMsg) {
+    display_location = errorMsg;
+  } else if (location) {
+    display_location= JSON.stringify(location);
+  }
+
   return (
-    <View>
-      <Text>Hello!</Text>
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{display_location}</Text>
       <Link href={"/(home)/Filters"}>Filters page</Link>
       <Link href={"/(home)/NewPin"}>New Pin page</Link>
       <Link href={"/(home)/Settings"}>Settings page</Link>
       <Link href={"/(home)/0"}> PinDetail page</Link>
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE}/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
