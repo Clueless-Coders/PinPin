b43e17bb6fa60e25e0b3c7061c7eff91ebfabfb6
trina314 <trina0dang@gmail.com>
2024-11-29 19:46:17 -0600

    completed some axiom methods and front end for testing

--- Mobile/app/(home)/[pinId].tsx
+++ Mobile/app/(home)/[pinId].tsx
@@ -1,31 +1,118 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { PinService } from "@/services/AuthService"; // Assume this is the service that provides getPin
-import { IPins } from "@/services/AuthService";
+import { TextInput } from "react-native";
+import { IPins, ICreatePin } from "@/services/AuthService";
 
 const pinService = new PinService();
 
 export default function PinDetail() {
   const [pin, setPin] = useState<IPins | null>(null);// State to store pin data
+  const [pinText, setText] = useState('');
+  const [pinID, setID] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [buttonValue, setButton] = useState(0);
+  const [requesting, isRequesting] = useState(false);
+  // const []
 
   useEffect(() => {
-    const fetchPin = async () => {
+    const updatePin = async () => {
       try {
-        const fetchedPin = await pinService.getPin(3); // Call the async function
-        setPin(fetchedPin); // Update state with fetched pin
+        let fetchedPin: IPins | undefined;
+        if (buttonValue == 0) {
+          return;
+        } else if (buttonValue == 1) {
+          fetchedPin = await pinService.deletePin(+pinID);
+        } else if (buttonValue == 2) {
+          fetchedPin = await pinService.getPin(+pinID); // Call the async function
+        } else if (buttonValue == 3) {
+          fetchedPin = await pinService.patchPin(+pinID, pinText);
+        } else if (buttonValue == 4) {
+          let newPin: ICreatePin = { text: pinText, latitude: +latitude, longitude: +longitude };
+          fetchedPin = await pinService.createPin(newPin);
+        }
+        setPin((fetchedPin !== undefined) ? fetchedPin : null); // Update state with fetched pin
+        setButton(0);
+        isRequesting(false);
       } catch (error) {
-        console.error("Error fetching pin:", error);
+        console.error("Error updating pin info", error);
       }
     };
 
-    fetchPin(); // Invoke the async function
+    updatePin(); // Invoke the async function
     console.log("inside useEffects");
-  }, []); // Empty dependency array to run this effect only once
+  }, [isRequesting]); // Empty dependency array to run this effect only once
 
   return (
     <View>
       <Text>Pin Detail Page</Text>
       {pin ? <Text>Pin: {JSON.stringify(pin)}</Text> : <Text>Loading...</Text>}
+
+      <TextInput
+        style={{
+          paddingTop: 10,
+          height: 40,
+          borderColor: "black",
+          borderRadius: 3,
+        }}
+        placeholder="pinID"
+        onChangeText={(val) => setID(val)}
+      ></TextInput>
+
+      <Button
+        title="Delete"
+        onPress={() => { setButton(1); isRequesting(true) }}
+        disabled={!isRequesting}
+      ></Button>
+
+      <Button
+        title="Get"
+        onPress={() => { setButton(2); isRequesting(true) }}
+        disabled={!isRequesting}
+      ></Button>
+
+      <TextInput
+        style={{ height: 40, borderColor: "black", borderRadius: 3 }}
+        placeholder="pin text"
+        onChangeText={(val) => setText(val)}
+      ></TextInput>
+
+      <TextInput
+        style={{
+          paddingTop: 10,
+          height: 40,
+          borderColor: "black",
+          borderRadius: 3,
+        }}
+        placeholder="latitude"
+        onChangeText={(val) => setLatitude(val)}
+      ></TextInput>
+
+      <TextInput
+        style={{
+          paddingTop: 10,
+          height: 40,
+          borderColor: "black",
+          borderRadius: 3,
+        }}
+        placeholder="longitude"
+        onChangeText={(val) => setLongitude(val)}
+      ></TextInput>
+
+
+      <Button
+        title="Patch"
+        onPress={() => { setButton(3); isRequesting(true) }}
+        disabled={!isRequesting}
+      ></Button>
+
+
+      <Button
+        title="Create"
+        onPress={() => { setButton(4); isRequesting(true) }}
+        disabled={!isRequesting}
+      ></Button>
     </View>
   );
 }
--- Mobile/services/AuthService.ts
+++ Mobile/services/AuthService.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 import { router } from "expo-router";
 import { jwtDecode } from "jwt-decode";
 
+
 export interface ITokens {
   access_token: string;
   refresh_token: string;
@@ -27,6 +28,13 @@ export interface IPins {
   latitude: number;
   createdAt: Date;
   updatedAt: Date;
+}
+
+export interface ICreatePin {
+  text: string;
+  imageURL?: string;
+  longitude: number;
+  latitude: number;
 }
 
 export class AuthService {
@@ -137,13 +145,52 @@ export class PinService {
   constructor() {
 
   }
-  async getPin(pinID: number): Promise<IPins> {
-    console.log("starting fetching")
-    const res = await axios.get<IPins>(`${API_BASE_URL}/pin/${pinID}`);
+  async getPin(pinID: number): Promise<IPins | undefined> {
+    console.log("starting fetching");
+    try {
+      const res = await axios.get<IPins>(`${API_BASE_URL}/pin/${pinID}`);
+      const pin = res.data;
+      console.log(pin);
+      return pin;
+    } catch (e) {
+      console.log("Failed to get pin");
+    }
 
-    var pin = res.data;
-    console.log(pin);
-    return pin;
+  }
 
+  async deletePin(pinID: number) {
+    console.log("deleting pin " + pinID);
+    try {
+      const res = await axios.delete<IPins>(`${API_BASE_URL}/pin/${pinID}`);
+      const pin = res.data;
+      console.log(pin);
+      return pin;
+    } catch (e) {
+      console.log("Failed to delete pin.");
+    }
+  }
+
+  async patchPin(pinID: number, text: string) {
+    console.log("updating pin " + pinID);
+    try {
+      const res = await axios.patch<IPins>(`${API_BASE_URL}/pin/${pinID}`, text);
+      const pin = res.data;
+      console.log(pin);
+      return pin;
+    } catch (e) {
+      console.log("failed to update pin");
+    }
+  }
+
+  async createPin(IPin: ICreatePin) {
+    console.log("creating pin ");
+    try {
+      const res = await axios.post<IPins>(`${API_BASE_URL}/pin`, IPin);
+      const pin = res.data;
+      console.log(pin);
+      return pin;
+    } catch (e) {
+      console.log("failed creating pin");
+    }
   }
 }
