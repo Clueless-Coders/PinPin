91509e89c5a1829d354abf8bbf7a36d6c58d423c
Caroline Peach <cpeach2@lsu.edu>
2024-12-01 01:08:43 -0600

    broken?

--- Mobile/app/(home)/index.tsx
+++ Mobile/app/(home)/index.tsx
@@ -9,42 +9,124 @@ import PinPost, { PinPostProps } from "@/components/PinPost";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons/faFilter";
-import { faGear } from "@fortawesome/free-solid-svg-icons/faGear";
-import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
 import { Link } from "expo-router";
-import SquareButton from "@/components/SquareButton";
 
 // TO DO
 // Finish search bar + filter button styling
+// Square Buttons Component
 // Modal Customizations (shadow, divider line, sticky top)
 // make keyboard work good
 // clean up code
 // the top component must be registered as viewing and change color
 
 export default function HomeIndex() {
-  const [data, setData] = useState<any>();
-  async function test() {
-    try {
-      const res = await axios.get(`${API_BASE_URL}/user/me`);
-      console.log(res.data);
-      setData(res.data);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  const sheetRef = useRef<BottomSheet>(null);
 
+  // testing pin content rendering, need to connect to backend eek
+  const data = useMemo(
+    () =>
+      Array(25)
+        .fill(0)
+        .map((_, index): PinPostProps & { id: string } => ({
+          isFocused: index === 0,
+          id: `index-${index}`,
+          distance: index + 1,
+          time: new Date(Date.now() - 30000),
+          text: `test pin index ${index} asldkfjasdkl;jasdkljdflk;asjfl;adjfkl;asdjfklasdfjasdfjasdklfjasjklasdfjasdkl;jasdjajasdjasdfjasdl;fjasdkfjasdfjasd`,
+          commentCount: index + 3,
+          karma: index + 4,
+        })),
+    []
+  );
+  // removed 100% snap point while nested scrolling doesnt work
+  const snapPoints = useMemo(() => ["12%", "75%"], []);
+
+  const handleSheetChange = useCallback((index: number) => {
+    console.log("handleSheetChange", index);
+  }, []);
+
+  // idk if we're gonna need this in the future
+  // const handleRefresh = useCallback(() => {
+  //   console.log("handleRefresh");
+  // }, []);
+
+  const renderItem = useCallback(({ item }: { item: PinPostProps }) => {
+    return (
+      <View style={{ marginBottom: 15 }}>
+        <PinPost
+          distance={item.distance}
+          time={item.time}
+          text={item.text}
+          commentCount={item.commentCount}
+          karma={item.karma}
+          isFocused={item.isFocused}
+        />
+      </View>
+    );
+  }, []);
   return (
     <GestureHandlerRootView style={styles.root}>
-      <SquareButton icon={faGear} color={"#FF6B6B"} route={"/(home)/Filters"} />
-      <SquareButton icon={faPlus} route={"/(home)/Filters"} />
-
       <Link href={"/(home)/Filters"}>Filters page</Link>
       <Link href={"/(home)/NewPin"}>New Pin page</Link>
       <Link href={"/(home)/Settings"}>Settings page</Link>
       <Link href={"/(home)/0"}> PinDetail page</Link>
 
-      <Button onPress={test} title="bonk"></Button>
-      {data ? <Text>{data.email}</Text> : undefined}
-    </View>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={snapPoints}
+        enableDynamicSizing={false}
+        onChange={handleSheetChange}
+        enableHandlePanningGesture={true}
+        enableOverDrag={false}
+        backgroundStyle={{ backgroundColor: "#FFF9ED" }}
+        handleIndicatorStyle={{ backgroundColor: "#000000" }}
+      >
+        <View style={styles.search}>
+          <BottomSheetTextInput style={styles.input} />
+          <FontAwesomeIcon icon={faFilter} />
+          {/* must add divider bar, try placing after filter instead of in view*/}
+        </View>
+
+        <BottomSheetFlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+          contentContainerStyle={styles.flatlist}
+          // refreshing={false}
+          // onRefresh={handleRefresh}
+          // nestedScrollEnabled={true}  // should enable nested scrolling but it doesnt work :(
+          // keyboardShouldPersistTaps="handled"  // why are taps still being blocked :(
+        />
+      </BottomSheet>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    paddingTop: 200,
+  },
+  flatlist: {
+    backgroundColor: "#FFF9ED",
+    marginVertical: 10,
+  },
+  input: {
+    marginHorizontal: 10,
+    marginTop: 8,
+    marginBottom: 15,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#000000",
+    fontSize: 16,
+    lineHeight: 20,
+    padding: 5,
+    color: "#FFF9ED",
+    width: 350, //not sure why this is stretching beyond the screen, must fix to be dynamic
+  },
+  search: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
