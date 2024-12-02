a967199107e498dbd0703f058c74918742973b44
Caroline Peach <cpeach2@lsu.edu>
2024-12-02 16:17:08 -0600

    saving

--- Mobile/app/(home)/[pinId].tsx
+++ Mobile/app/(home)/[pinId].tsx
@@ -1,10 +1,12 @@
+import { useLocalSearchParams } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 
 export default function PinDetail() {
+  const params = useLocalSearchParams();
   return (
     <View>
-      <Text>Pin Detail Page</Text>
+      <Text>{JSON.stringify(params)}</Text>
     </View>
   );
 }
--- Mobile/app/(home)/_layout.tsx
+++ Mobile/app/(home)/_layout.tsx
@@ -3,7 +3,17 @@ import React from "react";
 
 export default function HomeLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
       <Stack.Screen name="index" />
       <Stack.Screen name="Filters" />
       <Stack.Screen name="NewPin" />
