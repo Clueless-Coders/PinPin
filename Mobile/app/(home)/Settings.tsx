71f1f417f1068b6a39c4ad2a04b29a561972c020
TristanD0703 <111524307+TristanD0703@users.noreply.github.com>
2024-12-01 19:24:13 -0600

    Add textbox component

--- Mobile/app/(home)/Settings.tsx
+++ Mobile/app/(home)/Settings.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import { Text, View } from "react-native";
 
 export default function Settings() {
-  return (
-    <View>
-      <Text>Settings Page.</Text>
-    </View>
-  );
+  return <View></View>;
 }
