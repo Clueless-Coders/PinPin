82d604bfff5acb412872f28f40cdde6011c93d9c
Caroline Peach <cpeach2@lsu.edu>
2024-12-02 23:11:30 -0600

    leaving comment placeholder

--- Mobile/app/(home)/[pinId].tsx
+++ Mobile/app/(home)/[pinId].tsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import PinView, { PinPostProps } from "@/components/PinView";
 import Comment from "@/components/Comment";
 import { FlatList, GestureHandlerRootView } from "react-native-gesture-handler";
+import { TextInput, View, StyleSheet } from "react-native";
 
 export default function PinDetail() {
   // const [allViewablePins, setAllViewablePins] = useState<VisiblePin[]>([]);
@@ -10,7 +11,9 @@ export default function PinDetail() {
   const allViewablePins: PinPostProps[] = [
     { distance: 1, text: "First Comment", commentCount: 10, karma: 20 },
     { distance: 5, text: "Second Comment", commentCount: 5, karma: 15 },
-    { distance: 10, text: "Third Comment", commentCount: 3, karma: 12 },
+    { distance: 10, text: "Thrd Comment", commentCount: 3, karma: 12 },
+    { distance: 5, text: "Seond Comment", commentCount: 5, karma: 15 },
+    { distance: 10, text: "hird Comment", commentCount: 3, karma: 12 },
   ];
 
   const flatListRef = useRef<FlatList>(null);
@@ -44,13 +47,42 @@ export default function PinDetail() {
 
       <FlatList
         data={allViewablePins}
-        keyExtractor={(index) => index.toString()}
+        keyExtractor={(item, index) => index.toString()}
         renderItem={renderItem}
         ref={flatListRef}
         onScrollToIndexFailed={(info) => {
           console.log(info);
         }}
-      ></FlatList>
+      />
+      <View style={styles.textBoxContainer}>
+        <TextInput style={styles.textInput} placeholder="Leave a comment..." />
+      </View>
     </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  flatListContainer: {
+    paddingBottom: 70,
+  },
+  textBoxContainer: {
+    position: "absolute",
+    bottom: 0, 
+    left: 0,
+    right: 0,
+    backgroundColor: "orange", 
+    borderTopLeftRadius: 20, 
+    borderTopRightRadius: 20, 
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    zIndex: 1, 
+  },
+  textInput: {
+    height: 40,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+  },
+});
