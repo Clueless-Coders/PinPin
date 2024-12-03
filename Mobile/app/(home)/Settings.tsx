07db20c93b1b0d3903fb34168fcfc9038dfb07ba
Caroline Peach <cpeach2@lsu.edu>
2024-12-03 12:56:40 -0600

    created settings page, fixed setting button

--- Mobile/app/(home)/Settings.tsx
+++ Mobile/app/(home)/Settings.tsx
@@ -1,6 +1,43 @@
+import SettingsOptions from "@/components/SettingsOptions";
 import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 
 export default function Settings() {
-  return <View></View>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topSection}>
+        <SettingsOptions text="Notifications" icon="bell" />
+        <SettingsOptions text="Password and Email" icon="shield" />
+        <SettingsOptions text="Privacy Policy" icon="lock" />
+        <SettingsOptions text="Support" icon="user" />
+        <SettingsOptions text="Sign Out" icon="signOut" />
+        <SettingsOptions text="Contact Us" icon="envelope" />
+      </View>
+
+      <View style={styles.bottomSection}>
+        <SettingsOptions
+          text="Delete Account"
+          icon="trash"
+          onPress={() => alert("Delete Account")}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF9ED",
+    padding: 10,
+    marginTop: 20,
+  },
+  topSection: {
+    flex: 1,
+  },
+  bottomSection: {
+    justifyContent: "flex-end",
+    marginTop: 10,
+    marginBottom: 20,
+  },
+});
