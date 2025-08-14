3dd26a7a10f4c6a75080547a6a759a5588729e69
TristanD0703 <111524307+TristanD0703@users.noreply.github.com>
2025-08-13 22:50:47 -0500

    tiny changes for deploy

--- Mobile/app/home/create-pin/camera.tsx
+++ Mobile/app/home/create-pin/camera.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CameraView, useCameraPermissions } from 'expo-camera'
 import { Link } from "expo-router";
 import SquareButton from "@/components/SquareButton";
+import { ActivityIndicator } from "react-native";
 
 export default function Camera() {
     const [permission, requestPermission] = useCameraPermissions();
+
+    useEffect(() => {
+        if (permission && !permission.granted) {
+            requestPermission()
+        }
+    }, [])
+
+    // still loading permissions
+    if (!permission)
+        return <ActivityIndicator></ActivityIndicator>
+
     return (
         <CameraView style={{ flex: 1 }}>
             <Link href={'../'} asChild>
